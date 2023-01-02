13bc64f2b4b0bad1c9bfcf0858d9c0456446d95d
ybrenning <yannickbrenning2@gmail.com>
2023-01-02 18:24:33 +0100

    Add sign out option

--- spotimy.py
+++ spotimy.py
@@ -7,27 +7,42 @@ import spotipy
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 
-from data import (get_audio_features, get_genre_counts, get_genre_counts_data,
-                  get_loudness_energy_data, get_rank_popularity_data,
-                  get_top_artists, get_top_tracks)
+from data import (
+    get_audio_features,
+    get_genre_counts,
+    get_genre_counts_data,
+    get_loudness_energy_data,
+    get_rank_popularity_data,
+    get_top_artists,
+    get_top_tracks,
+)
 
-app = Flask(__name__, template_folder="chart/src/templates", static_folder="chart/src/static")
-app.config['SECRET_KEY'] = os.urandom(64)
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_FILE_DIR'] = './.flask_session/'
+app = Flask(
+    __name__, template_folder="chart/src/templates", static_folder="chart/src/static"
+)
+app.config["SECRET_KEY"] = os.urandom(64)
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_FILE_DIR"] = "./.flask_session/"
 Session(app)
 
 
 @app.route("/")
 def index():
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
-    auth_manager = spotipy.oauth2.SpotifyOAuth(scope=["user-top-read", "user-read-recently-played", "user-library-read","playlist-read-private"],
-                                                cache_handler=cache_handler,
-                                                show_dialog=True)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+        scope=[
+            "user-top-read",
+            "user-read-recently-played",
+            "user-library-read",
+            "playlist-read-private",
+        ],
+        cache_handler=cache_handler,
+        show_dialog=True,
+    )
     if request.args.get("code"):
         # Step 2. Being redirected from Spotify auth page
         auth_manager.get_access_token(request.args.get("code"))
-        return redirect('/')
+        return redirect("/")
 
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         # Step 1. Display sign in link when no token
@@ -37,7 +52,9 @@ def index():
 
     top_tracks, top_artists = get_top_tracks(sp), get_top_artists(sp)
 
-    loudness_energy_data = get_loudness_energy_data((get_audio_features(sp, top_tracks)))
+    loudness_energy_data = get_loudness_energy_data(
+        (get_audio_features(sp, top_tracks))
+    )
 
     genre_counts = get_genre_counts(top_artists)
     genre_counts_labels_overlap = sorted(
@@ -65,6 +82,18 @@ def index():
     )
 
 
+@app.route('/sign_out')
+def sign_out():
+    session.pop("token_info", None)
+    return redirect('/')
+
+
 if __name__ == "__main__":
-    app.run(threaded=True, port=int(os.environ.get("PORT",
-                                                   os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1])))
+    app.run(
+        threaded=True,
+        port=int(
+            os.environ.get(
+                "PORT", os.environ.get("SPOTIPY_REDIRECT_URI", 8080).split(":")[-1]
+            )
+        ),
+    )
